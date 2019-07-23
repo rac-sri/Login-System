30b9b32e8b752fa3a7f42358acce4a191b1eec41
rachit2501 <rac.sri25@gmail.com>
2019-07-24 01:13:42 +0530

    salt jwt left joi validation not working

--- index.js
+++ index.js
@@ -1,11 +1,61 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const body-parser = require('body-parser');
+const body = require('body-parser');
+const joi = require('joi');
 
-app.use(body-parser.json());
-app.use(body-parser.urlencoded({extended: true}));
+app.use(body.json());
+app.use(body.urlencoded({extended: true}));
 app.use(express.static('public'));
+
+
+//database
+	mongoose.connect('mongodb://localhost/user', {useNewUrlParser: true})
+	.then(()=>console.log('Connected to mongodb'))
+	.catch(err => console.error("Failed"));
+
+
+const schema = new mongoose.Schema({
+	name : String,
+	password: String
+});
+
+const data= new mongoose.model('User', schema);
+
+
+async function userData(req , res){
+	
+
+ const newuser= new data({
+ 		name : "safasdfgasdf",
+ 		password : "sksfzcczxcxz"
+ });
+
+
+//let result = validateuser(newuser);
+
+ //f(result){
+const result = await newuser.save();
+console.log(result);
+//}
+//else
+//console.log("Invalid Details");
+}
+
+
+
+//validation
+function validateuser(userd)
+{
+	
+	const sch = {
+		name: joi.string().min(2).max(20),
+		password : joi.string().min(5).max(30)
+	};
+
+	return result = joi.validate(userd , sch);
+}
+
 
 app.get('/', (req,res)=>
 {
@@ -13,6 +63,7 @@ app.get('/', (req,res)=>
 });
 
 app.post('/', (req , res)=>{
+	userData(req , res);
 	console.log(req.body);
 });
 app.listen(3000);
