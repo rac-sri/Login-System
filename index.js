ba07908684309dab02f0a4b22aa275df8b8b38f4
rachit2501 <rac.sri25@gmail.com>
2019-07-25 00:28:58 +0530

    form json obeject post handling on client side left

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const mongoose = require('mongoose');
 const body = require('body-parser');
 const joi = require('joi');
-//const auth = require('./views/js/auth');
+const auth = require('./views/js/auth');
+const jwt = require('jsonwebtoken');
+
 
 app.use(body.json());
 app.use(body.urlencoded({extended: true}));
@@ -33,11 +35,11 @@ async function userData(req , res){
 	if(pre) res.send('User already registered');
 
 else{
-	//let epass = auth(req.body.password)
+	let epass = await auth(req.body.password)
 
  const newuser= new data({
  		name : req.body.name,
- 		password :"sdalkjsakldjaskl"
+ 		password :epass
  });
 
 
@@ -46,6 +48,10 @@ else{
  //f(result){
 const result = await newuser.save();
 console.log(result);
+
+const signature = jwt.sign({_id: newuser.id} , "jwtpvtkey");
+res.header('x-auth-token', signature).send(newuser.id);
+
 }
 //}
 //else
@@ -55,16 +61,48 @@ console.log(result);
 
 
 //validation
-function validateuser(userd)
-{
+// function validateuser(userd)
+// {
 	
-	const sch = {
-		name: joi.string().min(2).max(20),
-		password : joi.string().min(5).max(30)
-	};
+// 	const sch = {
+// 		name: joi.string().min(2).max(20),
+// 		password : joi.string().min(5).max(30)
+// 	};
 
-	return result = joi.validate(userd , sch);
-}
+// 	return result = joi.validate(userd , sch);
+// }
+app.post('/me' , async (req , res)=>
+{
+	const token = req.header('x-auth-token');
+	if(!token)  res.status(401).send('Access denied.No token provided');
+
+	try{
+		const decoded = jwt.verify(token , 'jwtpvtkey');
+		req.user = decoded;
+		res.send('Successfull');
+	}
+	catch(ex)
+	{
+		res.status(400).send('Invalid token');
+	}
+
+});
+
+
+
+app.post('/login' , async (req , res)=>
+{
+const correct = data.findOne({name : req.body.name});
+if(!correct) return res.status(400).send("Invalid Username or Password");
+
+const passcomp = await bcrypt.compare(req.body.password , correct.password);
+if(!passcomp) return res.status(400).send("Invalid Username or Password");
+
+res.send("Sucessfule");
+
+
+});
+
 
 
 app.get('/', (req,res)=>
--- views/js/auth.js
+++ views/js/auth.js
@@ -1,1 +1,15 @@
-const bcrypt = require('brycpt');
+const bcrypt = require('bcrypt');
+
+async function salt(req)
+{
+	const salt = await bcrypt.genSalt(10);
+	const hash = await bcrypt.hash(req, salt);
+	return hash;
+} 
+
+module.exports = function auth(req)
+{
+	return salt(req);
+}
+
+
